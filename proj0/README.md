b70c4b4d80f3e202222d25a7d3da71d7b95efbbf
pgr-me <pgr-me@users.noreply.github.com>
2021-03-01 20:32:39 -0500

    Add example code and build from that

---
+++ proj0/README.md
@@ -0,0 +1,2 @@
+# ai_605202_lab1
+Data Structures Lab 1
---
+++ proj0/runtime_metric.py
@@ -0,0 +1,24 @@
+class RuntimeMetric:
+    def __init__(self, size: int, time_ns: float):
+        """
+        Holds data to determine Big-O runtime metrics.
+        :param size: Size of the problem
+        :param time_ns: Duration of the solution
+        """
+        self.size = size
+        self.time = time_ns
+
+    def get_runtime(self) -> float:
+        """
+        Fetches the time it took to solve the problem.
+        :return: The time measured in nanoseconds
+        """
+
+        return self.time
+
+    def get_size(self) -> int:
+        """
+        Fetches the size of the problem.
+        :return: A size that is determined by the way the problem is stated.
+        """
+        return self.size
